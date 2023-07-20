64b674b3ad5f68764e4ccd3b9586346615d45bf9
suma662001 <ksuma.xworkz@gmail.com>
2023-07-20 16:14:19 +0530

    animal

---
+++ git prog/Animals.java
@@ -0,0 +1,21 @@
+class Animals{
+
+public static void main (String []args)
+{
+	String gorila ="gorila";
+	String dog ="dog";
+	String cat="cat";
+	String monkey ="monkey";
+	String husband ="husband";
+	String  panda="panda";
+	String  donkey="donkey";
+
+
+	
+	String[] animals ={gorila,dog,cat ,monkey,husband,panda,donkey};
+	
+	String ref0 = animals[0];
+	System.out.println("print index valuve 0:"+ref0);
+	
+ }
+}
