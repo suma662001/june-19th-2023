10554009944142c28c27d6168977b2849eb3acdd
suma662001 <ksuma.xworkz@gmail.com>
2023-07-20 22:44:19 +0530

    park

---
+++ git prog/ParkRunner.java
@@ -0,0 +1,21 @@
+class ParkRunner
+{
+public static void main(String []args)
+{
+	System.out.println("invoking main in ParkRunner ");
+    Park park= new Park("rajkumar park",100,50,"rajajinagar");		
+		
+		
+		System.out.println("^^^^^^^^^^^^");
+    Park park1= new Park("rajkumar park",100,50,"rajajinagar",10);		
+		
+		System.out.println("+++++++++++++++++++++ ");
+    Park park2= new Park("rajkumar park",100,50,"rajajinagar",10,"playgame",200);		
+		
+		System.out.println("----------------------------");
+    Park park3= new Park("rajkumar park",100,50,"rajajinagar",10,"playgame",200,40,"winter");		
+		
+		
+		
+}
+}
